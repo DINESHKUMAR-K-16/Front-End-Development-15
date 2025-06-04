14cff2416c3e02ded6199b37ef6bde69558c9ed2
Dinesh Kumar K <160807494+DINESHKUMAR-K-16@users.noreply.github.com>
2025-06-04 07:31:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -14,51 +14,49 @@ navLinks.addEventListener("click", (e) => {
   menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
 
-const navSearch = document.getElementById("nav-search");
-
-navSearch.addEventListener("click", (e) => {
-  navSearch.classList.toggle("open");
-});
-
 const scrollRevealOption = {
   distance: "50px",
   origin: "bottom",
   duration: 1000,
 };
 
-ScrollReveal().reveal(".header__image img", {
+ScrollReveal().reveal(".header__container h1", {
   ...scrollRevealOption,
-  origin: "right",
 });
-ScrollReveal().reveal(".header__content div", {
-  duration: 1000,
+ScrollReveal().reveal(".header__container .section__description", {
+  ...scrollRevealOption,
   delay: 500,
 });
-ScrollReveal().reveal(".header__content h1", {
+ScrollReveal().reveal(".header__link", {
   ...scrollRevealOption,
   delay: 1000,
 });
-ScrollReveal().reveal(".header__content p", {
+
+ScrollReveal().reveal(".shop__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".about__content .section__header", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".about__content .section__description", {
+  ...scrollRevealOption,
+  delay: 500,
+  interval: 500,
+});
+ScrollReveal().reveal(".about__stats", {
   ...scrollRevealOption,
   delay: 1500,
 });
 
-ScrollReveal().reveal(".deals__card", {
-  ...scrollRevealOption,
-  interval: 500,
-});
-
-ScrollReveal().reveal(".about__image img", {
-  ...scrollRevealOption,
-  origin: "right",
-});
-ScrollReveal().reveal(".about__card", {
-  duration: 1000,
-  interval: 500,
-  delay: 500,
-});
-
 const swiper = new Swiper(".swiper", {
   loop: true,
+  slidesPerView: "auto",
+  spaceBetween: 20,
 });
 
+ScrollReveal().reveal(".banner__container p", {
+  duration: 1000,
+  interval: 500,
+});
