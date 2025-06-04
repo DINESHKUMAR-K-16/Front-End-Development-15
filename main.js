fe218f12fcc7419af43633817f0853adf11904b2
Dinesh Kumar K <160807494+DINESHKUMAR-K-16@users.noreply.github.com>
2025-06-04 07:37:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 const menuBtnIcon = menuBtn.querySelector("i");
 
-menuBtn.addEventListener("click", (e) => {
+menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("open");
 
   const isOpen = navLinks.classList.contains("open");
   menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
 });
 
-navLinks.addEventListener("click", (e) => {
+navLinks.addEventListener("click", () => {
   navLinks.classList.remove("open");
   menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
@@ -20,43 +20,51 @@ const scrollRevealOption = {
   duration: 1000,
 };
 
-ScrollReveal().reveal(".header__container h1", {
+// header container
+ScrollReveal().reveal(".header__container p", {
   ...scrollRevealOption,
 });
-ScrollReveal().reveal(".header__container .section__description", {
+
+ScrollReveal().reveal(".header__container h1", {
   ...scrollRevealOption,
   delay: 500,
 });
-ScrollReveal().reveal(".header__link", {
+
+// about container
+ScrollReveal().reveal(".about__image img", {
   ...scrollRevealOption,
-  delay: 1000,
+  origin: "left",
 });
 
-ScrollReveal().reveal(".shop__card", {
+ScrollReveal().reveal(".about__content .section__subheader", {
   ...scrollRevealOption,
-  interval: 500,
+  delay: 500,
 });
 
 ScrollReveal().reveal(".about__content .section__header", {
   ...scrollRevealOption,
+  delay: 1000,
 });
+
 ScrollReveal().reveal(".about__content .section__description", {
-  ...scrollRevealOption,
-  delay: 500,
-  interval: 500,
-});
-ScrollReveal().reveal(".about__stats", {
   ...scrollRevealOption,
   delay: 1500,
 });
 
-const swiper = new Swiper(".swiper", {
-  loop: true,
-  slidesPerView: "auto",
-  spaceBetween: 20,
+ScrollReveal().reveal(".about__btn", {
+  ...scrollRevealOption,
+  delay: 2000,
 });
 
-ScrollReveal().reveal(".banner__container p", {
-  duration: 1000,
+// room container
+ScrollReveal().reveal(".room__card", {
+  ...scrollRevealOption,
   interval: 500,
+});
+
+// service container
+ScrollReveal().reveal(".service__list li", {
+  ...scrollRevealOption,
+  interval: 500,
+  origin: "right",
 });
